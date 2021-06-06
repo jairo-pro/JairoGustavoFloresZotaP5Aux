31ceca86c461b1223eae6b2edcc95a36f9d5a4c9
Jairo G. Flores Z <jhairo4121@gmail.com>
2021-06-06 13:40:47 -0700

    levantando servidor

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,8 @@ const router = Router();
 router.get("/", (req: Request, res: Response) => {
   res.send(
     '<h1 style="margin: 80px 80px 0px 80px">' +
-      "your full name" +
-      "</h1>" +
-      '<p style="margin: 20px 0px 0px 80px">' +
-      "this is my Api" +
-      "</p>"
+      "Assignment 5, server running" +
+      "</h1>" 
   );
 });
 
